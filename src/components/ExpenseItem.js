53826d4ed0a667946d0626500bda02fbef2e86b6
Vineeth05kumar <Vineeth05kumar>
2024-05-26 12:53:19 +0530

    Added css to ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,14 @@
+import './ExpenseItem.css';
 
-export default function ExpenseItem () {
-    return (
-        <>
-        <h1>this is first commponent</h1>
-        </>
-    )
+export default function ExpenseItem() {
+  return (
+    <div className='expense-item'>
+       <div>14 August 2024</div>
+       <div className='expense-item__description'>
+        <h2>Book</h2>
+        <div className='expense-item__price'>$10</div>
+       </div>
+       <div className='expense-item__location'>Adilabad</div>
+    </div>
+  );
 }
