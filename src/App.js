2e6aa6974985892754b1860f774d1ad5ca886df0
Vineeth05kumar <Vineeth05kumar>
2024-05-26 14:18:44 +0530

    Passing data into ExpenseItem Using props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 export default function App() {
-
- 
   return (
     <div>
-      <ExpenseItem />
+      <ExpenseItem location="Banglore" name="Insurence" price="500" date="new Date(2024,5,23)" />
+      <ExpenseItem location="Delhi" name="Book" price="50" date="new Date(2024,5,23)"/>
+      <ExpenseItem location="Hyderbad" name="Pen" price="5" date="new Date(2024,5,23)"/>
+      <ExpenseItem location="Mumbai" name="Laptop" price="1500" date="new Date(2024,5,23)"/>
     </div>
   );
 }
