35255d0f0f14888587c00a045008889f26a444c1
Vineeth05kumar <Vineeth05kumar>
2024-05-31 15:48:15 +0530

    Submitted the form and logged the inputs as an object

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,26 +1,50 @@
 import "./ExpenseForm.css";
+import { useState } from "react";
 
 export default function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleHandler =(event) => {
-    console.log(event.target.value);
+    setEnteredTitle(event.target.value);
+  }
+
+  const amountHandler =(event) => {
+    setEnteredAmount(event.target.value);
+  }
+
+  const dateHandler =(event) => {
+    setEnteredDate(event.target.value);
+  }
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const obj = {
+      title:enteredTitle,
+      price:enteredAmount,
+      date:enteredDate,
+    }
+
+    console.log(obj);
   }
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input onChange={titleHandler} type="text" />
+          <input onChange={titleHandler} value={enteredTitle}type="text" />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" />
+          <input onChange={amountHandler} value={enteredAmount} type="number" />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2024-05-31" />
+          <input onChange={dateHandler} value={enteredDate} type="date" min="2019-01-01" max="2024-05-31" />
         </div>
         <div className="new-expense__actions">
-          <button>ADD EXPENSE</button>
+          <button type="submit" >ADD EXPENSE</button>
         </div>
       </div>
     </form>
